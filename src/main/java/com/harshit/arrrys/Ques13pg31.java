dcd44aa4851fa888063efe93c26ef12247e419c4
harshitmishra-90 <harshitmishra6450@gmail.com>
2026-07-06 21:14:22 +0530

    day-18

---
+++ src/main/java/com/harshit/arrrys/Ques13pg31.java
@@ -0,0 +1,53 @@
+package com.harshit.arrrys;
+
+public class Ques13pg31 {
+
+    public int longestSubarray(int[] nums) {
+        int left = 0;
+        int zeroCount = 0;
+        int maxLen = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+
+            if (nums[right] == 0) {
+                zeroCount++;
+            }
+
+            while (zeroCount > 1) {
+                if (nums[left] == 0) {
+                    zeroCount--;
+                }
+                left++;
+            }
+
+            maxLen = Math.max(maxLen, right - left);
+        }
+
+        return maxLen;
+    }
+
+    static void main() {
+
+    }
+}
+//Given a binary array nums, you should delete one element from it.
+//
+//Return the size of the longest non-empty subarray containing only 1's in the resulting array. Return 0 if there is no such subarray.
+//
+//
+//
+//Example 1:
+//
+//Input: nums = [1,1,0,1]
+//Output: 3
+//Explanation: After deleting the number in position 2, [1,1,1] contains 3 numbers with value of 1's.
+//Example 2:
+//
+//Input: nums = [0,1,1,1,0,1,1,0,1]
+//Output: 5
+//Explanation: After deleting the number in position 4, [0,1,1,1,1,1,0,1] longest subarray with value of 1's is [1,1,1,1,1].
+//Example 3:
+//
+//Input: nums = [1,1,1]
+//Output: 2
+//Explanation: You must delete one element.
