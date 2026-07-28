49233b1aaaabd9cc49cf8c5c1361d5905e1fdf52
harshitmishra-90 <harshitmishra6450@gmail.com>
2026-07-28 22:52:45 +0530

    day-35

---
+++ src/main/java/com/harshit/HashMap/ValidAnagram.java
@@ -0,0 +1,38 @@
+package com.harshit.HashMap;
+
+import java.util.HashMap;
+
+public class ValidAnagram {
+    public static boolean validanagram(String t,String s)
+    {
+        HashMap<Character,Integer> maps=new HashMap<>();
+        for(int i=0;i<s.length();i++)
+        {
+            maps.put(s.charAt(i),maps.getOrDefault(s.charAt(i),0)+1);
+        }
+        for(int i=0;i<t.length();i++)
+        {
+            char j=t.charAt(i);
+            if(maps.get(j)!=null)
+            {
+                if(maps.get(j)==1)
+                {
+                    maps.remove(j);
+                }
+                else {
+                    maps.put(j,maps.get(j)-1);
+                }
+            }
+            else{
+                return false;
+            }
+        }
+        return maps.isEmpty();
+
+    }
+    static void main() {
+        String s ="care";
+        String t="racce";
+        System.out.println(validanagram(t,s));
+    }
+}
