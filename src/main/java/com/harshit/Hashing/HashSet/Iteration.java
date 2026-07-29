56351cbef4a02e75ef6e9a10fa9b5a3a60015b5d
harshitmishra-90 <harshitmishra6450@gmail.com>
2026-07-29 23:25:46 +0530

    day-36

--- src/main/java/com/harshit/Hashing/HashSet/Iteration.java
+++ src/main/java/com/harshit/Hashing/HashSet/Iteration.java
@@ -2,6 +2,7 @@ package com.harshit.Hashing.HashSet;
 
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedHashSet;
 
 public class Iteration {
     static void main() {
@@ -10,6 +11,17 @@ public class Iteration {
         hashSet.add("USA");
         hashSet.add("NEPAL");
         hashSet.add("BHUTAN");
+        //unordered output
+        //means jaisa input kiya hai jaruri nhi ki vaisa output mile
+        System.out.println(hashSet);
+        LinkedHashSet<String> linkedHashSet=new LinkedHashSet<>();
+        linkedHashSet.add("INDIA");
+        linkedHashSet.add("USA");
+        linkedHashSet.add("NEPAL");
+        linkedHashSet.add("BHUTAN");
+        //ordered output
+        //output will be same as it inserted
+        System.out.println(linkedHashSet);
 
     }
 
