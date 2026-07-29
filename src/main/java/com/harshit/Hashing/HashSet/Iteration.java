b08764b3f324c13ddec3f1f23f09bef77fda2bd6
harshitmishra-90 <harshitmishra6450@gmail.com>
2026-07-29 23:37:01 +0530

    day-36

--- src/main/java/com/harshit/Hashing/HashSet/Iteration.java
+++ src/main/java/com/harshit/Hashing/HashSet/Iteration.java
@@ -3,6 +3,7 @@ package com.harshit.Hashing.HashSet;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedHashSet;
+import java.util.TreeSet;
 
 public class Iteration {
     static void main() {
@@ -23,6 +24,34 @@ public class Iteration {
         //output will be same as it inserted
         System.out.println(linkedHashSet);
 
+        //Treeset
+        //sorted in ascending order
+        //having the tc of O(logn) due to sorting
+
+        TreeSet<String>treeSet=new TreeSet<>();
+        treeSet.add("INDIA");
+        treeSet.add("USA");
+        treeSet.add("NEPAL");
+        treeSet.add("BHUTAN");
+        System.out.println(treeSet);
     }
 
 }
+//Why use an Iterator if println(hashSet) already prints everything?
+//
+//Using an iterator gives you more control. For example, you can:
+//
+//Process each element individually.
+//Skip certain elements.
+//Remove elements safely while iterating.
+//
+//        Example:
+//
+//Iterator<String> itr = hashSet.iterator();
+//
+//while (itr.hasNext()) {
+//String s = itr.next();
+//    if (s.startsWith("A")) {
+//        System.out.println(s);
+//    }
+//            }
