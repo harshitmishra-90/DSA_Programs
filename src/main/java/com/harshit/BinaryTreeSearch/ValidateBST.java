cca6d2f384b8c784e3d201a3fb9b2e4c5b1058cc
harshitmishra-90 <harshitmishra6450@gmail.com>
2026-08-10 19:20:32 +0530

    day-46

--- src/main/java/com/harshit/BinaryTreeSearch/ValidateBST.java
+++ src/main/java/com/harshit/BinaryTreeSearch/ValidateBST.java
@@ -55,6 +55,31 @@ public class ValidateBST {
         return root;
     }
 
+//    you are given the root of a binary search tree (BST) and an integer val.
+//
+//    Find the node in the BST that the node's value equals val and return the subtree rooted with that node. If such a node does not exist, return null.
+//
+//
+//
+//    Example 1:
+//
+//
+//    Input: root = [4,2,7,1,3], val = 2
+//    Output: [2,1,3]
+    public node searchBST(node root, int val) {
+        if(root==null)
+            return null;
+        if(root.data==val)
+        {
+            return root;
+        }
+        if(val<root.data)
+            return searchBST(root.left,val);
+        else
+            return searchBST(root.right,val);
+
+    }
+
     public static void main(String[] args) {
 
         int value[] = {8, 5, 3, 1, 4, 6, 10, 11, 14};
