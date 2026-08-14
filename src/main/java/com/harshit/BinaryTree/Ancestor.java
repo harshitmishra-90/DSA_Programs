e51672981a4eccd30699ce57255cbe2a1553f0a8
harshitmishra-90 <harshitmishra6450@gmail.com>
2026-08-15 01:45:33 +0530

    day-49

--- src/main/java/com/harshit/BinaryTree/Ancestor.java
+++ src/main/java/com/harshit/BinaryTree/Ancestor.java
@@ -3,6 +3,23 @@ package com.harshit.BinaryTree;
 
 //lowest comman ancestor
 public class Ancestor {
+    static class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+    }
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root==null||root.val==p.val||root.val==q.val)
+            return root;
+        TreeNode lca=lowestCommonAncestor(root.left,p,q);
+        TreeNode rla=lowestCommonAncestor(root.right,p,q);
+
+        if(lca==null)
+            return rla;
+        if(rla==null)
+            return lca;
+        return root;
+    }
     public static void main(String[] args) {
 
     }
