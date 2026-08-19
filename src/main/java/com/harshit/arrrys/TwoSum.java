84fa209ab2b5c449b2e8fcb138043af1db7be275
harshitmishra-90 <harshitmishra6450@gmail.com>
2026-08-19 23:22:36 +0530

    day-52

---
+++ src/main/java/com/harshit/arrrys/TwoSum.java
@@ -0,0 +1,23 @@
+package com.harshit.arrrys;
+
+import java.util.HashMap;
+
+public class TwoSum {
+    public static int[] sum(int arr[],int target)
+    {
+        HashMap<Integer,Integer> maps=new HashMap<>();
+        for(int i=0;i<arr.length;i++)
+        {
+            int needed=target-arr[i];
+            if(maps.containsKey(needed))
+                return new int[]{maps.get(needed),i};
+            maps.put(arr[i],i );
+        }
+        return new int[]{};
+    }
+    public static void main(String[] args) {
+        int arr[]={2,3,5};
+        System.out.println(sum(arr,5));
+
+    }
+}
